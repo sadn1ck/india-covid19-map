a4eac3bc86064c2720926feeefece056b7b63750
Anik Das <anikdas0811@gmail.com>
2020-04-15 16:56:44 +0530

    Displaying data about COVID on hovering on state

--- app.js
+++ app.js
@@ -12,12 +12,13 @@ map.setView([23, 82], 5);
 /*	Variety of base layers */
 var osm_mapnik = L.tileLayer('', {
   maxZoom: 10,
-  minZoom: 4,
+  minZoom: 5,
   attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 geojson = L.geoJson(india, {
-  style: style
+  style: style,
+  onEachFeature: onEachFeature
 }).addTo(map);
 
 function style(feature) {
@@ -47,5 +48,41 @@ function getColor(name) {
     if (name == coviddata[i]['state']) {
       return coviddata[i]['color']
     }
+  }
+}
+function getStateData(name){
+  for (i in coviddata) {
+    if (name == coviddata[i]['state']) {
+      return coviddata[i];
+    }
+  }
+}
+function onEachFeature(feature, layer) {
+  layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+      click: zoomToFeature
+  });
+}
+function zoomToFeature(e) {
+  map.fitBounds(e.target.getBounds());
+}
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+}
+function highlightFeature(e) {
+  var layer = e.target;
+  layer.setStyle({
+      weight: 5,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.7
+  });
+  var stateData = getStateData(layer.feature.properties.NAME_1);
+  document.getElementById("stat").innerHTML = "<h3>"+stateData['state']+"</h3><br>Confirmed: "+stateData['conf']+"<br>Deaths: "+stateData['deaths']+"<br>Recovered: "+stateData['cured']+"<br>";
+
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
   }
 }
